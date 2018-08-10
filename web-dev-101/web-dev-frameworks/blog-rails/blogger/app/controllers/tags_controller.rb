6fa8972a66a4f48d8a78a9b8ce1677e68f551dbe
Felipe Parreira <felipeparreirafb@gmail.com>
2018-08-10 10:04:53 -0300

    Add rails blog

---
+++ web-dev-101/web-dev-frameworks/blog-rails/blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,21 @@
+class TagsController < ApplicationController
+    def show
+        @tag = Tag.find(params[:id])
+    end
+
+    def index 
+        @tags = Tag.all
+    end
+
+    # prevent unauthenticated users from deleting the tags
+    before_action :require_login, only: [:destroy]
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+
+        flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+        redirect_to tags_path(@tag)
+    end
+end
